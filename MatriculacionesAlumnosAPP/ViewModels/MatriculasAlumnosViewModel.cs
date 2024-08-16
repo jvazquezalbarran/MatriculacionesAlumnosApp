1c7ecb8987d76f4c9dd1cadd1a44817ed902b9fa
javier <jvazquez@ucm.es>
2024-08-16 12:33:44 +0200

    Initial commit of ASP.Net

---
+++ MatriculacionesAlumnosAPP/ViewModels/MatriculasAlumnosViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MatriculacionesAlumnosAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatriculacionesAlumnosAPP.ViewModels
+{
+    public class MatriculasAlumnosViewModel
+    {
+        public List<Alumno> Alumnos { get; set; }
+        public int? AlumnoSeleccionado { get; set; }
+
+        public List<Asignatura> Asignaturas { get; set; }
+        public int? AsignaturaSeleccionada { get; set; }
+
+    }
+}
